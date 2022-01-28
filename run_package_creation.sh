b5408968a275401e3401392e9b0036f635fe5592
Thiago Franco de Moraes <totonixsame@gmail.com>
2022-01-28 16:27:30 -0300

    Compiling with openssl and updated version

--- run_package_creation.sh
+++ run_package_creation.sh
@@ -4,10 +4,10 @@ readonly gdcmroot="/gdcm"
 readonly gdcmsrc="$gdcmroot/src"
 readonly gdcmout="$gdcmroot/out"
 
-readonly baseimage="quay.io/pypa/manylinux2010_x86_64"
+readonly baseimage="quay.io/pypa/manylinux2014_x86_64"
 readonly container="gdcm-wheel"
 
 mkdir -p "$PWD/dist"
 
 docker pull "$baseimage"
-docker run -it --rm --workdir="$gdcmroot" --name="$container" -v "$PWD:$gdcmroot" "$baseimage" /bin/bash ./create_package_wheel.sh
+docker run -it --rm --workdir="$gdcmroot" --name="$container" -v "$PWD:$gdcmroot" "$baseimage" /bin/bash
