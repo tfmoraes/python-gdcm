d81dac047f6c626c9a4277b2aadb230f6dda0d87
Thiago Franco de Moraes <totonixsame@gmail.com>
2024-04-03 17:52:13 -0300

    created version 3.0.23.1

--- setup.py
+++ setup.py
@@ -191,7 +191,7 @@ class CMakeBuildExt(build_ext):
 
 setuptools.setup(
     name="python-gdcm",
-    version="3.0.23",
+    version="3.0.23.1",
     author="Thiago Franco de Moraes",
     author_email="totonixsame@gmail.com",
     description="Grassroots DICOM runtime libraries",
